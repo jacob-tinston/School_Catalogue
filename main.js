9e97e0d071e41ae9b8a1ac09069f6cbf9e59aaa1
jacob-tinston <jtinston678@gmail.com>
2021-03-11 16:13:59 +0000

    Task 16

--- main.js
+++ main.js
@@ -62,3 +62,4 @@ class HighSchool extends School {
 
 const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
 lorraineHansbury.quickFacts();
+lorraineHansbury.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
