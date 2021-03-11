664aa9cc6e799d1f78a6040354aaf7bd1774e805
jacob-tinston <jtinston678@gmail.com>
2021-03-11 15:43:29 +0000

    Task 1/2

---
+++ main.js
@@ -0,0 +1,7 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+};
